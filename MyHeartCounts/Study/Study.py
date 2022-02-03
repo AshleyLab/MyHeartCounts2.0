f2dd1573b2248dac75117c5d811a1f9b6ea5624f
Ali Javed <32461180+ali-javed@users.noreply.github.com>
2022-02-03 12:59:50 -0800

    Study Class added

---
+++ MyHeartCounts/Study/Study.py
@@ -0,0 +1,25 @@
+#python libraries needed in code
+
+class Study:
+    def __init__(self, SynapseConnection, AmazonConnection):
+        ##############################
+        # author: Ali Javed
+        # October: 3 February 2022
+        # email: ajaved@stanford.edu; alijaved@live.com
+        #############################
+        # Description:
+        
+        # Inputs
+        #
+        
+        #Output
+        #
+    
+        ########################################
+        
+    
+        
+        
+        
+        
+ 
