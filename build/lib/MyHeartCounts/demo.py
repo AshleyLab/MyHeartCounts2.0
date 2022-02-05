68b92293c9da3d0f416a7d5ef8d59171a5f6dc00
Ali Javed <32461180+ali-javed@users.noreply.github.com>
2022-02-05 15:28:18 -0800

    PyPi setup

---
+++ build/lib/MyHeartCounts/demo.py
@@ -0,0 +1,22 @@
+
+##############################
+# author: Ali Javed
+# October 3 Febuary 2022
+# Description: Scripts demonstrates how to use the MyHeartCounts Python package.
+# email: ajaved@stanford.edu; alijaved@live.com
+#############################
+
+
+
+#import libraries
+from MyHeartCounts import MyHeartCounts
+##############################
+
+#Initilize a MyHeartCounts object
+MHC = MyHeartCounts(user_password_file_path = '../../synapseAccess.txt')
+
+#Rev up your engine!!
+MHC.start()
+#load a study
+MHC.loadStudy(studyName = 'HealthKitWorkoutCollector',studyTable = 'syn3560095')
+
