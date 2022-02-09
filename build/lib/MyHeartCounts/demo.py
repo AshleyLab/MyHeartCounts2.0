cc18d65a7866d7d63cc01c048633e18dafcf68ef
Ali Javed <32461180+ali-javed@users.noreply.github.com>
2022-02-08 17:38:52 -0800

    PyPi library updates

--- build/lib/MyHeartCounts/demo.py
+++ build/lib/MyHeartCounts/demo.py
@@ -9,14 +9,31 @@
 
 
 #import libraries
-from myHeartCounts.MyHeartCounts import MyHeartCounts
+#from myHeartCounts.MyHeartCounts import MyHeartCounts
+from myHeartCounts import MyHeartCounts
+from datetime import datetime
 ##############################
 
 #Initilize a MyHeartCounts object
-MHC = MyHeartCounts(user_password_file_path = '../../synapseAccess.txt')
+MHC = MyHeartCounts(user_password_file_path = '../../synapseAccess.txt',synapseCachePath ='/Users/ajaved/Three/MHC_DataBase/code/synapseCache')
 
 #Rev up your engine!!
 MHC.start()
 #load a study
-MHC.loadStudy(studyName = 'HealthKitWorkoutCollector',studyTable = 'syn3560095')
+#MHC.loadStudy(studyName = 'mindset_illness',studyTable = ' syn18143712')
+#MHC.loadStudy(studyName = 'mindset_adequacy',studyTable = ' syn18143711')
+#MHC.loadStudy(studyName = 'mindset_exercise',studyTable = ' syn18143709')
+MHC.loadStudy(studyName = 'AB_TestResults',studyTable = 'syn7188351')
+#only download data for users we have in coacing study
 
+select_healthCodes = sorted(MHC.Studies[0].studyUsers)
+select_healthCodes = list(select_healthCodes)[1:5]
+MHC.loadStudy(studyName = 'HealthKitDataCollector',studyTable = 'syn3560085', healthCodes = [], silent = True, limit = 100000)
+
+
+print('retrieving blobs')
+MHC.Studies[1].retrieve_blobs(blob_names = ['data.csv'],healthCodes = select_healthCodes, silent=False)
+
+
+
+print('complete')
