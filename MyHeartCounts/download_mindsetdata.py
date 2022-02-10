c55a9cd59b8e7d6370074f7007cd555f028c3541
Ali Javed <32461180+ali-javed@users.noreply.github.com>
2022-02-10 10:46:53 -0800

    Filename float error

--- MyHeartCounts/download_mindsetdata.py
+++ MyHeartCounts/download_mindsetdata.py
@@ -9,10 +9,9 @@ MHC = MyHeartCounts(user_password_file_path='../synapseAccess.txt',
                     synapseCachePath='/Users/ajaved/Three/MHC_DataBase/code/synapseCache')
 # Rev up your engine!! -- Setting up of cache and other administrative scripts
 MHC.start()
-print('MHC ML Infrastructure up and running...')
 
 # load a studies
-MHC.loadStudy(studyName='HealthKitDataCollector', studyTable='syn3560085', limit = 1000)
+MHC.loadStudy(studyName='HealthKitDataCollector', studyTable='syn3560085', limit = 100)
 #MHC.loadStudy(studyName='mindset_adequacy', studyTable=' syn18143711')
 #MHC.loadStudy(studyName='AB_TestResults', studyTable='syn7188351')
 
@@ -22,6 +21,6 @@ users = MHC.Studies[0].studyUsers
 # we are down to 1044 users now. Let us see how much data they have in healthkit data collector. Lets start with 10 users just to check
 # download all data
 users = sorted(list(users))
-for i in range(1043, 0, -1):
-    MHC.Studies[0].retrieve_blobs(blob_names=['data.csv'], healthCodes=users[i:i + 50], silent=False)
+for i in range(len(users), 0, -1):
+    MHC.Studies[0].retrieve_blobs(blob_names=['data.csv'], healthCodes=users[i-2:i], silent=False)
     print(str(1044 - i) + ' of 1044 users downloaded.')
